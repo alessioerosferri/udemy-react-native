241cef336c984e5f3b46e07cf3ec1b00c69c5bd0
Alessio <alessioferriweb@gmail.com>
2020-04-29 22:14:40 +0100

    Rendering colours using state and flatlist

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,13 +1,31 @@
 import React, {useState} from "react";
-import {View, StyleSheet, Button} from "react-native";
+import {View, StyleSheet, Button, FlatList} from "react-native";
 
 const ColorScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [colours, setColours] = useState([]);
   return (
     <View>
-      <Button title="Add a colour"/>
+      <Button title="Add a colour" onPress={() => {
+        setColours([...colours, randomRgb()]);
+      }}/>
+      <FlatList
+        data={colours}
+        renderItem={({item}) => {
+          return <View style={{height: 100, width: 100, backgroundColor: item}}></View>;
+        }}
+        keyExtractor={(item) => {
+          return item;
+        }}
+      />
     </View>
   );
+}
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 const styles = StyleSheet.create({});
