f59114cb0eaae1633a3097fb5dc51dd6f74da102
Alessio <alessioferriweb@gmail.com>
2020-04-29 21:34:31 +0100

    Passing props down to image detail component

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
 import {View, Text, StyleSheet} from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
+  return (
+    <View>
+      <ImageDetail title="Forest" source={require("../../assets/forest.jpg")}/>
+      <ImageDetail title="Mountain" source={require("../../assets/mountain.jpg")}/>
+      <ImageDetail title="Beach" source={require("../../assets/beach.jpg")}/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
