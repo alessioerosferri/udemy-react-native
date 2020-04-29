c0f6a7c4f15e187f7e5703d0d37abd74dbf1f277
Alessio <alessioferriweb@gmail.com>
2020-04-29 18:00:39 +0100

    Using multiple styles groups

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,17 +2,20 @@ import React from "react";
 import {Text, StyleSheet, View} from "react-native";
 
 const ComponentsScreen = () => {
-  const greeting = "Hi there!";
+  const name = "Alessio";
   return (
     <View>
-      <Text style={styles.textStyle}>This is the Components screen</Text>
-      <Text>{greeting}</Text>
+      <Text style={styles.heading}>Getting started with React Native</Text>
+      <Text style={styles.paragraph}>My name is {name}.</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
+  heading: {
+    fontSize: 45
+  },
+  paragraph: {
     fontSize: 30
   }
 });
