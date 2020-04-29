e0fdf5beb6e379342dbdbfbe7169040ef9b612e3
Alessio <alessioferriweb@gmail.com>
2020-04-29 17:56:33 +0100

    Using View to render multiple children at the same Level

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import {Text, StyleSheet} from "react-native";
+import {Text, StyleSheet, View} from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the Components screen</Text>;
+  const greeting = "Hi there!";
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the Components screen</Text>
+      <Text>{greeting}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
