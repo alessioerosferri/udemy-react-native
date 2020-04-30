f0bf0313ab4493a9a020ee1a28bae789330ed422
Alessio <alessioferriweb@gmail.com>
2020-04-30 19:38:20 +0100

    Finalising square screen component

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, StyleSheet} from "react-native";
 import ColourCounter from "../components/ColourCounter";
 
+const ADJUSTER = 15;
+
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
   return (
     <View>
-      <ColourCounter colour="Red"/>
-      <ColourCounter colour="Green"/>
-      <ColourCounter colour="Blue"/>
+      <ColourCounter
+        onIncrease={() => setRed(red + ADJUSTER)}
+        onDecrease={() => setRed(red - ADJUSTER)}
+        colour="Red"
+      />
+      <ColourCounter
+        onIncrease={() => setGreen(green + ADJUSTER)}
+        onDecrease={() => setGreen(green - ADJUSTER)}
+        colour="Green"
+      />
+      <ColourCounter
+        onIncrease={() => setBlue(blue + ADJUSTER)}
+        onDecrease={() => setBlue(blue - ADJUSTER)}
+        colour="Blue"
+      />
+      <View style={{width: 150, height: 150, backgroundColor: `rgb(${red},${green},${blue})`}}>
+      </View>
     </View>
   );
 }
