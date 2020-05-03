e2f9ee9dad9754d9100e517e5aad81868c6c20e8
Alessio <alessioferriweb@gmail.com>
2020-05-03 19:14:43 +0100

    testing flex-end

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 1,
     borderColor: "black",
-    alignItems: "flex-start"
+    alignItems: "flex-end"
   },
   textStyle: {
     borderWidth: 3,
