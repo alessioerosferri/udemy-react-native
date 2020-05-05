591f535ef84987e4a869a2c96678b5859a0e7def
Alessio <alessioferriweb@gmail.com>
2020-05-05 19:11:45 +0100

    Handling user input and storing value in state. Using hook to set State

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,16 @@ import React from "react";
 import {View, TextInput, StyleSheet} from "react-native";
 import {Feather} from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({term, onTermChange}) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle}/>
-      <TextInput style={styles.inputStyle} placeholder="Search"/>
+      <TextInput
+        value={term}
+        onChangeText={onTermChange}
+        style={styles.inputStyle}
+        placeholder="Search"
+      />
     </View>
   );
 }
