852b9a63a533ab9a16fb8118cf5d3906c9326450
Alessio <alessioferriweb@gmail.com>
2020-05-05 19:23:18 +0100

    Adding auth and api key

--- food/src/apis/yelp.js
+++ food/src/apis/yelp.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const API_KEY = "ObfWXVkyA5pHVAgreEkn4D5AFbqhGjrkKD_G0ivQZ4pkKulLOdLW35zKrhtSUVyPynYrOkYcRyGYzGhWSFM3mkJMwL1-xQGzVBIGiZ-T2HANsCVoPzwl6HTheWKwXnYx"
+
 export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses"
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization: `Bearer ${API_KEY}`
+  }
 });
