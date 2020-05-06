c88317b1a7b937a4a4440560264ed69de79e5229
Alessio <alessioferriweb@gmail.com>
2020-05-06 20:16:53 +0100

    Rendering the pic

--- food/src/components/ResultDetail.js
+++ food/src/components/ResultDetail.js
@@ -3,12 +3,26 @@ import {Image, Text, View, StyleSheet} from "react-native";
 
 const ResultDetail = ({result}) => {
   return (
-    <View>
-      <Text>{result.name}</Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: result.image_url}}/>
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15
+  },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {
+    fontWeight: "bold"
+  }
+});
 
 export default ResultDetail;
--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -23,7 +23,8 @@ const ResultsList = ({title, results}) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    marginLeft: 15
   }
 });
 
