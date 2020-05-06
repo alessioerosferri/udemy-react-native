3a293e6f9ef6a89e522f56467c7939df7e012037
Alessio <alessioferriweb@gmail.com>
2020-05-06 20:05:05 +0100

    Creating ResultDetail.js

---
+++ food/src/components/ResultDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Image, Text, View, StyleSheet} from "react-native";
+
+const ResultDetail = () => {
+  return (
+    <View>
+      <Text>Result Detail</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultDetail;
