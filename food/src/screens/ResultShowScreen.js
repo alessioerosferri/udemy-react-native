3587d12fd828f8e52e4b09522eaccc203ce996b8
Alessio <alessioferriweb@gmail.com>
2020-05-07 16:46:03 +0100

    Init detail component page

---
+++ food/src/screens/ResultShowScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+
+const ResultShowScreen = () => {
+  return (
+    <View>
+      <Text>Results show</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultShowScreen;
