156c12356b115d7e230f805ff35bb0cdf2975b94
Alessio <alessioferriweb@gmail.com>
2020-05-07 19:48:50 +0100

    Refactoring using hook useReducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,12 +1,23 @@
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_post":
+      return [...state, {title: `Blog Post #${state.length + 1}`}];
+    default:
+      return state;
+  }
+}
+
 export const BlogProvider = ({children}) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, [], undefined);
+
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
-  };
+    dispatch({type: "add_post"});
+  }
+
   return <BlogContext.Provider value={{blogPosts, addBlogPost}}>
     {children}
   </BlogContext.Provider>
