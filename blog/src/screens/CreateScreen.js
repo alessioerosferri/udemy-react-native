26dc3a98019a5463033b7bc9e9d18078c71f92b8
Alessio <alessioferriweb@gmail.com>
2020-05-07 21:19:54 +0100

    Creating a CreateScreen.js

---
+++ blog/src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React, {useContext} from "react";
+import {View, StyleSheet, Text} from "react-native";
+
+const CreateScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>CreateScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
