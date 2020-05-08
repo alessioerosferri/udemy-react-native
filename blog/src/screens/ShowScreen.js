71ceeb3617cdb29fc4787c66e3d4d6d45d13933d
Alessio <alessioferriweb@gmail.com>
2020-05-08 11:54:19 +0100

    Adding navigation for editing a blog post

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, {useContext} from "react";
-import {View, StyleSheet, Text} from "react-native";
+import {View, StyleSheet, Text, TouchableOpacity} from "react-native";
 import {Context} from "../context/BlogContext";
+import {EvilIcons} from "@expo/vector-icons";
 
 const ShowScreen = ({navigation}) => {
   const id = navigation.getParam("id");
@@ -10,8 +11,19 @@ const ShowScreen = ({navigation}) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+}
+
+ShowScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: () => {
+      return <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <EvilIcons name="pencil" size={30}/>
+      </TouchableOpacity>;
+    }
+  }
 }
 
 const styles = StyleSheet.create({});
