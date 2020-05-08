14bef3a837f2ce6fc1b96b127afd690ff81f35d7
Alessio <alessioferriweb@gmail.com>
2020-05-08 14:03:13 +0100

    Using useEffect to call action creator that fetches the list of blogPosts from API

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,11 +1,16 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from "react-native";
 import {Feather} from "@expo/vector-icons";
 
 import {Context} from "../context/BlogContext";
 
 const IndexScreen = ({navigation}) => {
-  const {state, deleteBlogPost} = useContext(Context);
+  const {state, deleteBlogPost, getBlogPosts} = useContext(Context);
+
+  useEffect(() => {
+    getBlogPosts()
+  }, []);
+
   return (
     <View>
       <FlatList
