8e2f780e83066977b5f46d329245260c138fc525
Alessio <alessioferriweb@gmail.com>
2020-05-09 18:12:10 +0100

    Getting API to understando JSON requests

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri = "mongodb+srv://admin:admin@cluster0-iq8l6.mongodb.net/test?retryWrites=true&w=majority"
