e7fb922c9bd1ac7a904a7f059b70738387c5b7b0
Alessio <alessioferriweb@gmail.com>
2020-05-09 18:13:23 +0100

    beautify

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
-  res.send("You made a POST request")
+  res.send("You made a POST request");
 });
 
 module.exports = router;
