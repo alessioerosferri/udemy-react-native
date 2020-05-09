a956bfec75a6d22d1f81313b816fea427cd933ce
Alessio <alessioferriweb@gmail.com>
2020-05-09 18:03:35 +0100

    Connecting to mongo instance through mongoose

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri = "mongodb+srv://admin:admin@cluster0-iq8l6.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!")
