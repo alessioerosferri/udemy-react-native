de7b77cce4cd07b948495c42f578c4060e5dea4e
Alessio <alessioferriweb@gmail.com>
2020-05-12 20:20:17 +0100

    Removing header and adding some styling

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -5,16 +5,30 @@ import Spacer from "../components/Spacer";
 
 const SignupScreen = ({navigation}) => {
   return (
-    <>
+    <View style={styles.container}>
       <Spacer><Text h3>Sign Up for Tracker</Text></Spacer>
       <Input label="Email"/>
       <Spacer/>
       <Input label="Password"/>
       <Spacer><Button title="Sign Up"/></Spacer>
-    </>
+    </View>
   )
 }
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: "red",
+    borderWidth: 10,
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200
+  }
+});
 
 export default SignupScreen;
