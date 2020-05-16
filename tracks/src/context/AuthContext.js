1909cf5e13cb8bcfb7efed4e5b72fb6ee2927374
Alessio <alessioferriweb@gmail.com>
2020-05-16 23:41:31 +0100

    Init signup action creator

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,6 +7,10 @@ const authReducer = (state, action) => {
   }
 }
 
+const signup = dispatch => ({email, password}) => {
+
+}
+
 export const {Provider, Context} = createDataContext(
   authReducer,
   {},
