b0c3ecb9882ca280cc51c0c490e21c44e4d75156
Alessio <alessioferriweb@gmail.com>
2020-05-31 18:59:25 +0100

    Handling storing token in state

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -6,6 +6,8 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return {...state, errorMessage: action.payload}
+    case "signup":
+      return {...state, token: action.payload}
     default:
       return state;
   }
@@ -14,7 +16,9 @@ const authReducer = (state, action) => {
 const signup = dispatch => async ({email, password}) => {
   try {
     const response = await trackerAPI.post("/signup", {email, password});
-    await AsyncStorage.setItem("token", response.data.token);
+    const {token} = response.data
+    await AsyncStorage.setItem("token", token);
+    dispatch({type: "signup", payload: token});
   } catch (e) {
     dispatch({type: "add_error", payload: "Something went wrong with sign up"});
   }
@@ -31,5 +35,5 @@ const signout = dispatch => () => {
 export const {Provider, Context} = createDataContext(
   authReducer,
   {signup, signin, signout},
-  {isSignedIn: false, errorMessage: ""}
+  {token: null, errorMessage: ""}
 )
